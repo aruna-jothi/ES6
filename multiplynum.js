0374517d11bb3cff0dbd3c13aec5da66fd54fe23
aruna-jothi <74009511+aruna-jothi@users.noreply.github.com>
2022-01-13 17:17:29 +0530

    Add files via upload

---
+++ multiplynum.js
@@ -0,0 +1,25 @@
+
+const multiply = (n1,n2,n3)=>n1*n2*n3;
+let z = multiply(2,3,4);
+console.log("Multiplication of given number is: ", z);
+
+if(isNaN(z)){
+    console.log(z, "is not a number");
+}
+else
+{
+    console.log(z, "is a number");
+}
+
+let x = "Hi Welcome";
+if(isNaN(x)){
+    console.log(x, "is not a number");
+
+}
+else
+{
+    console.log(x, "is  a number");
+
+}
+
+
---
+++ spread.js
@@ -0,0 +1,6 @@
+const arr1=[1,2,3];
+const arr2=[6,7];
+const arr3=['hi', 'welcome'];
+
+const mergeResult = [...arr1, ...arr2,...arr3];
+console.log(mergeResult);
